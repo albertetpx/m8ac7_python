ede52ad70e83ac3b2e15e6390017d1a94bc958b0
albertetpx <aguardiola@etpx.fjaverianas.com>
2022-11-12 10:09:49 +0100

    Added css link

--- m8ac7.py
+++ m8ac7.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template;
+from flask import __version__,Flask, render_template;
+import sys;
 
 app = Flask(__name__)
 
 @app.route('/') 
 def hello_world():
-   return 'Hello from Flask!' 
+   pyversion = sys.version_info[0]
+   flaskversion = __version__
+   return render_template("index.html",
+        pythonversion = pyversion,
+        flaskversion=flaskversion) 
 
 if __name__ == '__main__':
    app.run()
