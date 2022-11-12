6124598b4255a57e5723b82095d45739bfcb0b2e
albertetpx <aguardiola@etpx.fjaverianas.com>
2022-11-12 10:59:36 +0100

    Added mysql conn

--- m8ac7.py
+++ m8ac7.py
@@ -1,10 +1,20 @@
 from flask import __version__,Flask, render_template;
 import sys;
+import mysql.connector
 
 app = Flask(__name__)
 
+def registerConnection():
+   db = mysql.connector.connect(user='root', password='',
+                              host='localhost',
+                              database='m8ac7')
+   cursor = db.cursor()
+   cursor.execute("insert into prova() values();")
+   db.close()
+
 @app.route('/') 
 def hello_world():
+   registerConnection()
    pyversion = sys.version_info[0]
    flaskversion = __version__
    return render_template("index.html",
