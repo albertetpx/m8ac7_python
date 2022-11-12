87ce76af4e8d36fa9d5505164ee10c2ff36afa9a
albertetpx <aguardiola@etpx.fjaverianas.com>
2022-11-12 11:12:00 +0100

    Connection fix

--- m8ac7.py
+++ m8ac7.py
@@ -5,8 +5,8 @@ import mysql.connector
 app = Flask(__name__)
 
 def registerConnection():
-   db = mysql.connector.connect(user='root', password='',
-                              host='192.168.1.134',
+   db = mysql.connector.connect(user='usuari1', password='',
+                              host='localhost',
                               database='m8ac7')
    cursor = db.cursor()
    cursor.execute("insert into prova;")
